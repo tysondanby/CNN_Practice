32920eb8d41bf92e8cc01dfe92e84350e792b8f7
tysondanby <tyson.danby@hotmail.com>
2025-02-07 22:49:26 -0700

    Training

--- PROCESSING/camera/train_model.jl
+++ PROCESSING/camera/train_model.jl
@@ -22,53 +22,76 @@ cd(working_dir)
 using Metalhead, Flux, MLUtils, Images, CSV, DataFrames, CUDA, cuDNN, JLD2
 CUDA.allowscalar(false)
 include(working_dir*"/LIB/imageprocessing.jl")
-previously_loaded = false
 
-function lpnormpoolinglayer(x)#causes issues with GPU
-    return lpnormpool(x,Float32(0.5),(3,3)) ./ Float32(81)
-end
-
-function getfirstlayer()
-    return Parallel(function f(a,b,c) return cat(a,b,c;dims=(3)) end,MaxPool((3,3)),MeanPool((3,3)),Conv((3,3), 1 => 1; stride = 3))
-end
-
-
-function getlossfunc()
-  return  function lossfunction(yhat,y)
-            return sum((yhat .- y).^2)
-          end
-end
-
-function updatetrainingepochsbybatchcsv(nbatches::T) where T<: Integer
-  csvname ="PROCESSING/camera/trainingepochsbybatch.csv"
-  historycsv = CSV.File(csvname)
-  newbatches = nbatches - length(historycsv.batch)
-  if newbatches > 0
-    lastbatchnumber = historycsv.batch[end]
-    for i = 1:1:newbatches
-      push!(historycsv.batch,lastbatchnumber+i)
-      push!(historycsv.epochs,0)
-    end
-    CSV.write(csvname,DataFrame(historycsv))
+function to_cuda(x)
+  if x isa Array
+      return cu(x)  # Convert CPU arrays to CUDA arrays
+  elseif x isa NamedTuple
+      return NamedTuple{keys(x)}(to_cuda.(values(x)))  # Recursively process NamedTuple
+  elseif x isa Dict
+      return Dict(k => to_cuda(v) for (k, v) in x)  # Process Dicts recursively
+  else
+      return x  # Leave other types unchanged
   end
 end
 
-function updatetrainingepochsbybatchcsv(batchestrained,epochstrainedeach)
-  csvname ="PROCESSING/camera/trainingepochsbybatch.csv"
-  historycsv = CSV.File(csvname)
-  for (i, batchnumber) in enumerate(batchestrained)
-    historycsv.epochs[batchnumber] = historycsv.epochs[batchnumber] + epochstrainedeach[i]
-  end
-  CSV.write(csvname,DataFrame(historycsv))
+function lossfunction(yhat,y)
+  return sum((yhat .- y).^2)
 end
 
-function zerotrainingepochsbybatchcsv()
-  csvname ="PROCESSING/camera/trainingepochsbybatch.csv"
-  historycsv = CSV.File(csvname)
-  for i = 1:1:length(historycsv.epochs)
-    historycsv.epochs[i] = 0
+function getnewestmodelstate()
+  save_state_filenames = readdir("PROCESSING/camera/model_states/")
+  save_state_numbers = zeros(Int64,length(save_state_filenames))
+  for i = 1:1:length(save_state_numbers)
+    save_state_numbers[i] = parse(Int64,save_state_filenames[i][1:end-5])
   end
-  CSV.write(csvname,DataFrame(historycsv))
+  return save_state_filenames[findfirst(x->x == maximum(save_state_numbers),save_state_numbers)], maximum(save_state_numbers)
+end
+
+
+function getmodel(;previously_loaded = false)
+  pretrainmodel = ResNet(18; pretrain = !previously_loaded)
+  pretrainedbackbone = pretrainmodel.layers[1]
+  firstlayer = Parallel(function f(a,b,c) return cat(a,b,c;dims=(3)) end,
+                        MaxPool((3,3)),
+                        MeanPool((3,3)),
+                        Conv((3,3), 1 => 1; stride = 3))
+  lastlayers = Chain(AdaptiveMeanPool((1, 1)),
+                    MLUtils.flatten,
+                    Dense(512 => 50),
+                    Dense(50 => 6))
+  modelstructure = Chain(firstlayer,pretrainedbackbone,lastlayers)
+  if previously_loaded == false
+    return modelstructure
+  else
+    newest, ~ = getnewestmodelstate()
+    model_state = JLD2.load("PROCESSING/camera/model_states/"*newest, "model_state")
+    Flux.loadmodel!(modelstructure, model_state)
+    return modelstructure
+  end
+end
+
+function getsimplemodel(weightdecay,clipnorm,learningrate;previously_loaded = false)
+  modelstructure = Chain(
+                          Conv((3,3),1 => 4;stride = 3), 
+                          Conv((5,5),4 => 16;stride = 4,pad = SamePad()), Dropout(0.1; rng = CUDA.RNG()),
+                          Conv((7,7),16 => 64;stride = 4,pad = SamePad()), Dropout(0.05; rng = CUDA.RNG()),
+                          AdaptiveMeanPool((1, 1)),MLUtils.flatten,
+                          Dense(64 => 20), Dropout(0.02; rng = CUDA.RNG()),
+                          Dense(20 => 6)
+                          )
+  c_modelstructure = cu(modelstructure)
+  c_opt_state = Flux.setup(OptimiserChain(WeightDecay(weightdecay), ClipNorm(clipnorm), Adam(learningrate)),c_modelstructure)
+  if previously_loaded == false
+    return c_modelstructure, c_opt_state
+  else
+    newest, ~ = getnewestmodelstate()
+    model_state = JLD2.load("PROCESSING/camera/model_states/"*newest, "model_state")
+    opt_state = JLD2.load("PROCESSING/camera/opt_states/opt_state.jld2","c_opt_state")
+    Flux.loadmodel!(modelstructure, model_state)
+    #Flux.loadmodel!(c_opt_state, opt_state) #TODO: load in opt state
+    return c_modelstructure, c_opt_state
+  end
 end
 
 function gettrainingdatabatch(batchsize,batchnumber,datacsv)
@@ -123,98 +146,24 @@ end
 function gettrainingdatainbatches(;batchsize = 16, set = "train")#Batchsize MUST be divisible by 8
   datacsv = CSV.File("DATA/camera/model_data/"*set*".csv")
   nbatches = Int32(floor((length(datacsv.filename)-1)*8/batchsize))
-  updatetrainingepochsbybatchcsv(nbatches)
   return gettrainingdatabatches(nbatches,batchsize,datacsv)
 end
 
-function findnewestmodelstatefilename()
-  save_state_filenames = readdir("PROCESSING/camera/model_states/")
-  save_state_numbers = zeros(Int64,length(save_state_filenames))
-  for i = 1:1:length(save_state_numbers)
-    save_state_numbers[i] = parse(Int64,save_state_filenames[i][1:end-5])
-  end
-  return save_state_filenames[findfirst(x->x == maximum(save_state_numbers),save_state_numbers)] 
-end
-
-function findnewestmodelstatenumber()
-  save_state_filenames = readdir("PROCESSING/camera/model_states/")
-  save_state_numbers = zeros(Int64,length(save_state_filenames))
-  for i = 1:1:length(save_state_numbers)
-    save_state_numbers[i] = parse(Int64,save_state_filenames[i][1:end-5])
-  end
-  return maximum(save_state_numbers)
-end
-
-function getmodel(;previously_loaded = false)
-  pretrainmodel = ResNet(18; pretrain = !previously_loaded)
-  pretrainedbackbone = pretrainmodel.layers[1]
-  firstlayer = getfirstlayer()
-  lastlayers = Chain(AdaptiveMeanPool((1, 1)),MLUtils.flatten,Dense(512 => 50),Dense(50 => 6))
-  modelstructure = Chain(firstlayer,pretrainedbackbone,lastlayers)
-  if previously_loaded == false
-    zerotrainingepochsbybatchcsv()
-    return modelstructure
-  else
-    newest = findnewestmodelstatefilename()
-    model_state = JLD2.load("PROCESSING/camera/model_states/"*newest, "model_state")
-    Flux.loadmodel!(modelstructure, model_state)
-    return modelstructure
-  end
-end
-
-function getsimplemodel(;previously_loaded = false)
-  modelstructure = Chain(
-                          Conv((3,3),1 => 4;stride = 3),
-                          Conv((5,5),4 => 16;stride = 4,pad = SamePad()),
-                          Conv((7,7),16 => 64;stride = 4,pad = SamePad()),
-                          AdaptiveMeanPool((1, 1)),MLUtils.flatten,
-                          Dense(64 => 20),
-                          Dense(20 => 6)
-                          )
-  if previously_loaded == false
-    #zerotrainingepochsbybatchcsv()
-    return modelstructure
-  else
-    newest = findnewestmodelstatefilename()
-    model_state = JLD2.load("PROCESSING/camera/model_states/"*newest, "model_state")
-    Flux.loadmodel!(modelstructure, model_state)
-    return modelstructure
-  end
-end
-
-function getnumberepochsperbatch(nepochs,epochchunksize)
-  nchunks = floor(nepochs/epochchunksize)
-  remainderepochs = nepochs - (epochchunksize*nchunks)
-  historycsv = CSV.File("PROCESSING/camera/trainingepochsbybatch.csv")
-  batches_old = historycsv.batch
-  epochs = deepcopy(historycsv.epochs)
-
-  minindex = sortperm(epochs)[1]
-  batches    = [ batches_old[minindex] ]
-  epochseach = [ remainderepochs ]
-  epochs[minindex] = epochs[minindex] + remainderepochs
-  chunk = 1
-  while chunk <= nchunks
-    minindex = sortperm(epochs)[1]
-    push!(batches,batches_old[minindex])
-    push!(epochseach,epochchunksize)
-    epochs[minindex] = epochs[minindex] + epochchunksize
-    chunk = chunk +1
-  end
-  return batches, epochseach
-end
-
-function getmodelreadytotrain(resetmodel)
-  model = getsimplemodel(;previously_loaded = !resetmodel)#TODO
-  trainingdatainput, trainingdataoutput = gettrainingdatainbatches(; set = "train")
-  verificationdatainput, verificationdataoutput = gettrainingdatainbatches(; set = "verify")
-  lossfunc = getlossfunc()
-  #includes regularization #Flux.setup(Adam(), model)
-  return model, trainingdatainput, trainingdataoutput, verificationdatainput, verificationdataoutput, lossfunc
+function modelanddatatoRAM(resetmodel,weightdecay,clipnorm,learningrate; batchsize = 32)
+  println("Moving objects to memory - Model (0/3)")
+  model,opt_state = getsimplemodel(weightdecay,clipnorm,learningrate;previously_loaded = !resetmodel)
+  println("Moving objects to memory - Training Data (1/3)")
+  trainingdatainput, trainingdataoutput = gettrainingdatainbatches(; set = "train", batchsize = batchsize)
+  println("Moving objects to memory - Verification Data (2/3)")
+  verificationdatainput, verificationdataoutput = gettrainingdatainbatches(; set = "verify", batchsize = batchsize)
+  lossfunc = deepcopy(lossfunction)
+  println("Moving objects to memory - Done! (3/3)")
+  return model,opt_state,trainingdatainput, trainingdataoutput, verificationdatainput, verificationdataoutput, lossfunc
 end
 
 function savemodelstate(model)
-  modelstatenumber = findnewestmodelstatenumber() + 1
+  ~,newestmodelstatenumber = getnewestmodelstate()
+  modelstatenumber = newestmodelstatenumber + 1
   filename = lpad(modelstatenumber,6,"0")*".jld2"
   model_state = Flux.state(cpu(model))
   jldsave("PROCESSING/camera/model_states/"*filename; model_state)
@@ -222,15 +171,11 @@ function savemodelstate(model)
 end
 
 function totaldatasetloss(model, lossfunc, trainingdatainput, trainingdataoutput)
-  total = Float32(0.0)#TODO: check if model is on GPU
-  for batchnumber = 1:1:size(trainingdatainput)[4]
+  total = Float32(0.0)
+  for batchnumber = 1:1:size(trainingdatainput)[3]
     currentbatchtrainingdataoutput = cu(trainingdataoutput[:,:,batchnumber])
     currentbatchtrainingdatainput = cu(trainingdatainput[:,:,batchnumber:batchnumber,:])
-    #for itemnumber = 1:1:size(trainingdatainput)[3]
     total = total + lossfunc(model(currentbatchtrainingdatainput), currentbatchtrainingdataoutput)
-    #end
-    #CUDA.unsafe_free!(currentbatchtrainingdataoutput)
-    #CUDA.unsafe_free!(currentbatchtrainingdatainput)
   end
   return total / (size(trainingdataoutput)[2]*size(trainingdataoutput)[3])
 end
@@ -247,68 +192,70 @@ function updatelossbyepochcsv(nepochs, model, trainingdatainput, trainingdataout
   CSV.write("PROCESSING/camera/lossbyepoch.csv",DataFrame(csv))
 end
 
-function trainbatch(currentbatchtrainingdatainput,currentbatchtrainingdataoutput,lossfunc,model,opt_state)
-  #for i = 1:1:size(currentbatchtrainingdatainput)[3]
-    ##=
-    grads = Flux.gradient(model) do m
-      #result = m(currentbatchtrainingdatainput[:,:,i:i,:])
-      lossfunc(m(currentbatchtrainingdatainput[:,:,:,:]), currentbatchtrainingdataoutput[:,:])#x,y,1(was batchnumber),itemnumber     out,itemnumber,  wasbatchnumber
-    end
-    Flux.update!(opt_state, model, grads[1])
-    # =#
- # end
-end
-
 function batchgrads(currentbatchtrainingdatainput,currentbatchtrainingdataoutput,lossfunc,model)
   grads = Flux.gradient(model) do m
-    #result = m(currentbatchtrainingdatainput[:,:,i:i,:])
     lossfunc(m(currentbatchtrainingdatainput[:,:,:,:]), currentbatchtrainingdataoutput[:,:])#x,y,1(was batchnumber),itemnumber     out,itemnumber,  wasbatchnumber
   end
   return grads
 end
 
-function trainmodel(nseries,nepochs;resetmodel = false,epochchunksize = 1)
+function trainepoch!(c_opt_state, c_model,trainingdatainput,trainingdataoutput,c_lossfunc)
+  accum_grads = nothing
+  re = nothing
+  for batchnumber = 1:1:size(trainingdatainput)[3]
+    currentbatchtrainingdataoutput = cu(trainingdataoutput[:,:,batchnumber])
+    currentbatchtrainingdatainput = cu(trainingdatainput[:,:,batchnumber:batchnumber,:])
+    grads = batchgrads(currentbatchtrainingdatainput,currentbatchtrainingdataoutput,c_lossfunc,c_model)
+    flat_grads, re = Flux.destructure(grads)
+    if accum_grads == nothing
+      accum_grads = flat_grads
+    else
+      accum_grads .+= flat_grads
+    end
+  end
+  Flux.update!(c_opt_state, c_model, re(accum_grads)[1])
+end
+
+function cleanVRAM()
+  println("\n \n"*"Freeing unused GPU rescources")
+  GC.gc()
   CUDA.reclaim()
-  reset = deepcopy(resetmodel)
-  model, trainingdatainput, trainingdataoutput, verificationdatainput, verificationdataoutput, lossfunc = getmodelreadytotrain(resetmodel)
-  nbatches = size(trainingdatainput)[3]
-  nbatchestrainedperseries = nbatches * nepochs
-  #batchnumbers, epochseach = getnumberepochsperbatch(nbatchestrainedperseries,epochchunksize)
-  c_model = cu(model)
-  c_lossfunc = cu(lossfunc) #                       1e-4            1.0        0.001
-  opt_state = Flux.setup(OptimiserChain(WeightDecay(1e-4), ClipNorm(1.0), Adam(0.001)), c_model)
-  println("Starting training")
+end
+
+function loadoptstate(resetmodel, c_model, optimizer)
+  c_opt_state = nothing
+  if resetmodel == true
+    return Flux.setup(optimizer, c_model)
+  else
+    @load "PROCESSING/camera/opt_states/opt_state.jld2" c_opt_state
+    return to_cuda(c_opt_state)
+  end
+end
+
+function trainmodel(nseries,nepochs;resetmodel = false,batchsize = 16, weightdecay = 1e-4, clipnorm = 1.0, learningrate = 0.005)
+  CUDA.reclaim()
+  c_model, c_opt_state, trainingdatainput, trainingdataoutput, verificationdatainput, verificationdataoutput, lossfunc = modelanddatatoRAM(resetmodel,weightdecay,clipnorm,learningrate;batchsize = batchsize)
+  println("Moving model to GPU - (0/1)")
+  #c_model = cu(model)
+  c_lossfunc = cu(lossfunc)
+
+  # = loadoptstate(resetmodel, c_model, OptimiserChain(WeightDecay(weightdecay), ClipNorm(clipnorm), Adam(learningrate)))
+  println("Moving model to GPU - Done! (1/1)")
   totalepochcounter = 0
   for i = 1:1:nseries
-    CUDA.reclaim()
+    cleanVRAM()
     epochcounter = 0
+    println("Initiating Training")
     for j = 1:1:nepochs
-      accum_grads = nothing #TODO: may be a better way of initializing this that doesn't necesitate if statement
-      re = nothing
-      for batchnumber = 1:1:nbatches
-        currentbatchtrainingdataoutput = cu(trainingdataoutput[:,:,batchnumber])
-        currentbatchtrainingdatainput = cu(trainingdatainput[:,:,batchnumber:batchnumber,:])
-        grads = batchgrads(currentbatchtrainingdatainput,currentbatchtrainingdataoutput,c_lossfunc,c_model)
-        flat_grads, re = Flux.destructure(grads)
-        if accum_grads == nothing
-          accum_grads = flat_grads
-        else
-          accum_grads .+= flat_grads
-        end
-      end
-      Flux.update!(opt_state, c_model, re(accum_grads)[1])
+      trainepoch!(c_opt_state, c_model,trainingdatainput,trainingdataoutput,c_lossfunc)
       epochcounter = epochcounter + 1
       totalepochcounter = totalepochcounter + 1
       println("Completed $epochcounter / $nepochs epochs in series $i / $nseries. ( $totalepochcounter / $(nseries*nepochs) )")
     end
+    println("Saving model checkpoint.")
     modelstatenumber = savemodelstate(c_model)
-    updatelossbyepochcsv(Int32(floor(nbatchestrainedperseries/nbatches)), c_model, trainingdatainput, trainingdataoutput, verificationdatainput, verificationdataoutput, c_lossfunc, modelstatenumber)
-    #updatetrainingepochsbybatchcsv(batchnumbers, epochseach)
-    #model = cpu(c_model)
-    #c_model = nothing
-    #opt_state = nothing
-    GC.gc()
-    #CUDA.reclaim()
-    reset = false
+    @save "PROCESSING/camera/opt_states/opt_state.jld2" c_opt_state
+    println("Model checkpoint saved. Calculating and recording model losses.")
+    updatelossbyepochcsv(nepochs, c_model, trainingdatainput, trainingdataoutput, verificationdatainput, verificationdataoutput, c_lossfunc, modelstatenumber)
   end
 end
